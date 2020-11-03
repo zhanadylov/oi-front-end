9580ce8697e6bb56421c831b1037d59e2c8e8e37
marsel <zhanadylov.kg@gmal.com>
2020-11-03 14:39:54 +0600

    Наименование компании

--- src/components/ReportList.vue
+++ src/components/ReportList.vue
@@ -368,7 +368,7 @@ export default {
         {
           key: 'id',
           headerTitle: '№',
-          label: '№'
+          label: 'Наименование компании'
         },
         // {
         //   key: 'id',
